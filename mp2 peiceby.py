83cce623a31ae77d5cdcabf527ba2d2a5ee3fa13
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-09-29 16:45:21 -0500

    breakdowns

---
+++ mp2 peiceby.py
@@ -0,0 +1,9 @@
+""" 
+Christian Nelson
+9/29/2020
+CPSC-57100-002, Fall 2020
+Machine Problem 2: Tic-Tac-Toe
+"""
+
+print("CLASS: Artificial Intelligence, Lewis University")
+print("NAME: Christian Nelson")
