04991b38d4a434538ab46a324e419e283e6be847
ecn3 <43419606+ecn3@users.noreply.github.com>
2020-09-30 15:25:45 -0500

    working random

--- mp2 comment.py
+++ mp2 comment.py
@@ -2,7 +2,7 @@
 Christian Nelson
 9/29/2020
 CPSC-57100-002, Fall 2020
-Machine Problem 2: Tic-Tac-Toe
+Machine Problem 2: Alpha/Beta Search for Generalized Tic-Tac-Toe
 """
 
 
@@ -10,11 +10,12 @@ import numpy as np
 import random
 import math
 
+# Class creates Gameboard
 class GenGameBoard: 
     def __init__(self, boardSize):
         self.boardSize = boardSize
-        self.marks = np.empty((boardSize, boardSize),dtype='str')
-        self.marks[:,:] = ' '
+        self.marks = np.empty((boardSize, boardSize),dtype='str') # array of where the X and Os are
+        self.marks[:,:] = ' ' # Initalize as empty no moves
 
     def printBoard(self): 
         print(' ',end='')
@@ -42,7 +43,7 @@ class GenGameBoard:
         
         print("-")
     
-    def makeMove(self, row, col, mark):
+    def makeMove(self, row, col, mark): # make a mark either x or o
         possible = False
         if row==-1 and col==-1:
             return False
@@ -50,21 +51,21 @@ class GenGameBoard:
         row = row - 1
         col = col - 1
         
-        if row<0 or row>=self.boardSize or col<0 or col>=self.boardSize:
+        if row<0 or row>=self.boardSize or col<0 or col>=self.boardSize: # checks if that is a valid position
             print("Not a valid row or column!")
             return False
 
-        if self.marks[row][col] == ' ':
-            self.marks[row][col] = mark
+        if self.marks[row][col] == ' ': # checks if the spot is taken
+            self.marks[row][col] = mark # sets the new mark
             possible = True    
 
-        if not possible and mark=='X':
+        if not possible and mark=='X': # if the position is taken by X it returns not available
             print("\nself position is already taken!")
         
-        return possible
+        return possible # return if move is possible
     
-    def checkWin(self, mark):
-        won = False 
+    def checkWin(self, mark): # checks to see if a win has been made
+        won = False # return variable
 
         for i in range(self.boardSize):
             won = True
@@ -101,12 +102,38 @@ class GenGameBoard:
 
         return won
 
-    def noMoreMoves(self):
+    def noMoreMoves(self): # checks if board is full
         return (self.marks!=' ').all()
 
     
     # TODO
     def makeCompMove(self):
+        # where we put maximizer
+        # run alapha beta search
+
+        """ 
+        function ALPHA-BETA-SEARCH(state) returns an action
+            v ←MAX-VALUE(state,−∞,+∞)
+            return the action in ACTIONS(state) with value v
+
+        function MAX-VALUE(state,α, β) returns a utility value
+            if TERMINAL-TEST(state) then return UTILITY(state) # determine if state is terminal or not, a terminal state is when the board is full, or when min or max wins it is terminal
+            v ←−∞
+            for each a in ACTIONS(state) do
+                v ←MAX(v, MIN-VALUE(RESULT(s,a),α, β))
+                if v ≥ β then return v
+                α←MAX(α, v)
+            return v
+        function MIN-VALUE(state,α, β) returns a utility value
+            if TERMINAL-TEST(state) then return UTILITY(state)
+            v ←+∞
+            for each a in ACTIONS(state) do
+                v ←MIN(v, MAX-VALUE(RESULT(s,a) ,α, β))
+                if v ≤ α then return v
+                β←MIN(β, v)
+            return v
+            """
+        # Currently makes random move
         row, col = -1, -1
         while not self.makeMove(row, col, 'O'):
             col = random.randint(1,boardSize)
--- mp2 peiceby.py
+++ mp2 peiceby.py
@@ -5,5 +5,18 @@ CPSC-57100-002, Fall 2020
 Machine Problem 2: Tic-Tac-Toe
 """
 
+class GenGameBoard: 
+    print("Delete me after making class")
+
+
+# Print out the header info
 print("CLASS: Artificial Intelligence, Lewis University")
 print("NAME: Christian Nelson")
+
+# Constants and base variables
+LOST = 0
+WON = 1
+DRAW = 2    
+wrongInput = False
+boardSize = int(input("Please enter the size of the board n (e.g. n=3,4,5,...): ")) # Get boardSize from player
+
